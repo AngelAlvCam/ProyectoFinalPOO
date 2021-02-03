ba4335a5883961b746dfa8909b806fdbabb7b12f
Flutt <Flutt@192.168.0.102>
2021-02-02 22:07:50 -0600

    Corrección de un pequeño error
    En versiones anteriores un método "CDC" de la clase "Carrera_de_caballos
    había pasado desapercibido. El método se renombró como "Jugar" para
    estandarizar la notación en los menus de usuario

--- src/Juegos/Carrera_de_caballos.java
+++ src/Juegos/Carrera_de_caballos.java
@@ -1,7 +1,7 @@
 package Juegos;
 
 public class Carrera_de_caballos {
-	public static boolean CDC () {
+	public static boolean Jugar () {
 		System.out.println("Eso es una carrera de caballos");
 		return true;
 	}
